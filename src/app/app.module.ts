142ee37e14c2d6568f37119bf0617b59206edda6
sachin <sachin14051@gmail.com>
2023-08-28 14:50:36 +0530

    working on ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './shared/components/log-in/log-in.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+
   ],
   imports: [
     BrowserModule
